c5244c8dbc5782dbf1bcb0f0c372924b372d0129
Katy Chan <cchan@my.bcit.ca>
2023-03-12 22:38:52 -0700

    add foundcourse loop

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 
-let courseList = [
+let courselist = [
     { code: "ACIT 1620", name: "Web Fundamental Technologies" },
     { code: "ACIT 1515", name: "Scripting for IT" },
     { code: "ACIT 1630", name: "Database Systems" },
@@ -10,3 +10,20 @@ let usernumber;
 do {
     usernumber = prompt("Please enter a 4-digit number: ");
 } while (isNaN(usernumber) === true || usernumber.length!=4);
+
+
+
+let foundCourse = false;
+
+for (let i = 0; i < courselist.length; i++) {
+    if (courselist[i].code.includes(usernumber)) {
+      console.log(`Yes I am taking the course: ${courselist[i].code} - ${courselist[i].name}`);
+      foundCourse = true;
+      break;
+    }
+}
+
+if(foundCourse === false){ 
+    courselist.push({code: usernumber, name: null });
+    console.log(`Successfully adding new course: ${usernumber}!`);
+}
