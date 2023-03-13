7e75a8c1ac65034f98e8f7b25096f5fd6c099587
Katy Chan <cchan@my.bcit.ca>
2023-03-12 22:02:07 -0700

    add array courselist in js file

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+
+let courseList = [
+    { code: "ACIT 1620", name: "Web Fundamental Technologies" },
+    { code: "ACIT 1515", name: "Scripting for IT" },
+    { code: "ACIT 1630", name: "Database Systems" },
+];
+
+
