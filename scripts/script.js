6316bab8b0284f739a8cb209d839768103e2e593
Katy Chan <cchan@my.bcit.ca>
2023-03-12 22:55:13 -0700

    modify code

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 
-let courselist = [
+const courselist = [
     { code: "ACIT 1620", name: "Web Fundamental Technologies" },
     { code: "ACIT 1515", name: "Scripting for IT" },
     { code: "ACIT 1630", name: "Database Systems" },
