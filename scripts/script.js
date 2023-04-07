0be0679612b6cdee4d3a0836c8acad522abb189c
Katy Chan <cchan@my.bcit.ca>
2023-04-06 22:48:04 -0700

    add func findCourseList to js file

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,37 @@ const courselist = [
 
 
 
+
+function findCourseList(){
+
+    let usernumber;
+
+    do {
+        usernumber = prompt("Please enter a 4-digit number: ");
+    } while (isNaN(usernumber) === true || usernumber.length!=4);
+
+
+    let foundCourse = false;
+
+    for (let i = 0; i < courselist.length; i++) {
+        if (courselist[i].code.includes(usernumber)) {
+        console.log(`Yes I am taking the course: ${courselist[i].code} - ${courselist[i].name}`);
+        document.querySelector(`.ACIT${usernumber}`).classList.add("selected");
+        foundCourse = true;
+        break;
+        }
+    }
+
+    if(foundCourse === false){ 
+        courselist.push({code: usernumber, name: "N/A" });
+        console.log(`Successfully adding new course: ${usernumber}!`);
+    };
+
+};
+
+findCourseList();
+
+
 const CourseArray = [];
     
 
